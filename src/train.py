cb5e984b7250e59e462a1cb5d22a0e3052c7d8d6
Barney Hill <barneyh@NDPH9082.local>
2024-04-18 17:18:21 +0100

    added pLLR calc for single mut

--- src/train.py
+++ src/train.py
@@ -281,3 +281,30 @@ def get_feature_matrix(wt_seq) -> np.ndarray:
 
             return uniprot_feature_matrix
     return None
+
+tok_to_idx = {'<cls>': 0, 
+              '<pad>': 1, 
+              '<eos>': 2, 
+              '<unk>': 3, 
+              'L': 4, 'A': 5, 'G': 6, 'V': 7, 'S': 8, 
+              'E': 9, 'R': 10, 'T': 11, 'I': 12, 'D': 13, 
+              'P': 14, 'K': 15, 'Q': 16, 'N': 17, 'F': 18, 
+              'Y': 19, 'M': 20, 'H': 21, 'W': 22, 'C': 23, 
+              'X': 24, 'B': 25, 'U': 26, 'Z': 27, 'O': 28, 
+              '.': 29, '-': 30, '<null_1>': 31, '<mask>': 32}
+
+idx_to_tok = {v:k for k,v in tok_to_idx.items()}
+
+def get_pLLR(mutant, logits):
+    # mutation is str, example: 'A123L'
+    # logits is a tensor of shape (seq_length, num_tokens)
+    wt_token_idx = mutated_position_idx = tok_to_idx[mutant[0]]
+    mut_token_idx = mutated_position_idx = tok_to_idx[mutant[-1]]
+
+    mutated_position_idx = mutant[1:-1] - 1
+
+    pLLR = np.log10(logits[mutated_position_idx, mut_token_idx] / logits[mutated_position_idx, wt_token_idx])
+    
+    return pLLR
+
+
