e264c47d5795424a62ca4404544d17fe80b9335b
Qurat ul ain <114662059+qurat-ul-ain95@users.noreply.github.com>
2024-04-18 17:26:08 +0100

    Update model.py

--- src/model.py
+++ src/model.py
@@ -2,10 +2,32 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+class EarlyStopping:
+    def __init__(self, patience=5, verbose=False, delta=0):
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.delta = delta
 
+    def __call__(self, val_loss):
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            if self.verbose:
+                print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.counter = 0
+
+            
+            
 class ProteinModel(nn.Module):
     def __init__(self):
         super(ProteinModel, self).__init__()
